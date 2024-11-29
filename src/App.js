af24dab917e044857914c2c828a82834bd924c17
WonderSTK <mehulparmar9694@gmail.com>
2024-11-29 15:18:37 +0530

    modification

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
+//import { useSelector } from 'react-redux';
 import Dashboard from './components/Dashboard';
 
 function App() {
-  const darkMode = useSelector((state) => state.theme.darkMode);
+  //const darkMode = useSelector((state) => state.theme.darkMode);
 
-  useEffect(() => {
-    document.documentElement.classList.toggle('dark', darkMode);
-  }, [darkMode]);
+ // useEffect(() => {
+  //  document.documentElement.classList.toggle('dark', darkMode);
+ // }, [darkMode]);
 
   return (
-    <div className={`min-h-screen ${darkMode ? 'dark' : ''}`}>
+    <div className={`min-h-screen }`}>
       <Dashboard />
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countryReducer from './slices/countrySlice';
-import themeReducer from './slices/themeSlice';
+//import themeReducer from './slices/themeSlice';
 import dataReducer from './slices/dataSlice';
 
 export const store = configureStore({
   reducer: {
     country: countryReducer,
-    theme: themeReducer,
     data: dataReducer,
   },
 });
